e23061b5bed4023dc319327252d9de77060584dd
raffaele-pilloni <raffaele.pilloni@hotmail.it>
2024-01-06 23:40:03 +0100

    Add service tests create task fail

--- test/internal/service/task_service_test.go
+++ test/internal/service/task_service_test.go
@@ -2,10 +2,12 @@ package service_test
 
 import (
 	"context"
+	"errors"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	clientdto "github.com/raffaele-pilloni/axxon-test/internal/client/dto"
 	"github.com/raffaele-pilloni/axxon-test/internal/entity"
+	applicationerror "github.com/raffaele-pilloni/axxon-test/internal/error"
 	"github.com/raffaele-pilloni/axxon-test/internal/service"
 	servicedto "github.com/raffaele-pilloni/axxon-test/internal/service/dto"
 	dmock "github.com/raffaele-pilloni/axxon-test/mock"
@@ -128,8 +130,8 @@ var _ = Describe("Task Service Tests", func() {
 			mock.MatchedBy(func(actualRequestDTO *clientdto.RequestDTO) bool {
 				return actualRequestDTO.Method == task.MethodToString() &&
 					actualRequestDTO.URL == task.URL &&
-					reflect.DeepEqual(actualRequestDTO.Body, task.RequestBodyToJSON()) &&
-					reflect.DeepEqual(actualRequestDTO.Headers, task.RequestHeadersToMap())
+					reflect.DeepEqual(actualRequestDTO.Headers, task.RequestHeadersToMap()) &&
+					reflect.DeepEqual(actualRequestDTO.Body, task.RequestBodyToJSON())
 			}),
 		).Return(&responseDTO, nil)
 
@@ -137,7 +139,7 @@ var _ = Describe("Task Service Tests", func() {
 			"Save",
 			context,
 			mock.MatchedBy(func(actualTask *entity.Task) bool {
-				return actualTask.Status == entity.StatusInProcess &&
+				return actualTask.Status == entity.StatusDone &&
 					reflect.DeepEqual(actualTask.ResponseHeadersToMap(), responseDTO.Headers) &&
 					actualTask.ResponseStatusCodeToInt() == responseDTO.StatusCode &&
 					actualTask.ResponseContentLengthToInt() == len(responseDTO.Body)
@@ -147,9 +149,97 @@ var _ = Describe("Task Service Tests", func() {
 		task, err := taskService.ProcessTask(context, task)
 		Ω(err).To(BeNil())
 
-		Ω(task.StatusToString()).To(Equal(string(entity.StatusInProcess)))
+		Ω(task.StatusToString()).To(Equal(string(entity.StatusDone)))
 		Ω(task.ResponseHeadersToMap()).To(Equal(responseDTO.Headers))
 		Ω(task.ResponseStatusCodeToInt()).To(Equal(responseDTO.StatusCode))
 		Ω(task.ResponseContentLengthToInt()).To(Equal(len(responseDTO.Body)))
+	})
+
+	It("should fail create task when method is not allowed", func() {
+		context := context.Background()
+
+		createRequestDTO := servicedto.CreateTaskDTO{
+			Method: "INVALID",
+			URL:    "http://test.com",
+			Headers: map[string][]string{
+				"test": {"test"},
+			},
+			Body: map[string]interface{}{
+				"test": "test",
+			},
+		}
+
+		task, err := taskService.CreateTask(context, &createRequestDTO)
+		Ω(err).ToNot(BeNil())
+		Ω(task).To(BeNil())
+
+		invalidMethodForTaskError, ok := err.(*applicationerror.InvalidMethodForTaskError)
+		Ω(ok).To(BeTrue())
+
+		Ω(invalidMethodForTaskError.Message()).To(Equal("The method INVALID is not valid for task."))
+		Ω(invalidMethodForTaskError.Code()).To(Equal(applicationerror.InvalidMethodForTaskErrorCode))
+	})
+
+	It("should fail create task when url is invalid", func() {
+		context := context.Background()
+
+		createRequestDTO := servicedto.CreateTaskDTO{
+			Method: "POST",
+			URL:    "invalid-test.com",
+			Headers: map[string][]string{
+				"test": {"test"},
+			},
+			Body: map[string]interface{}{
+				"test": "test",
+			},
+		}
+
+		task, err := taskService.CreateTask(context, &createRequestDTO)
+		Ω(err).ToNot(BeNil())
+		Ω(task).To(BeNil())
+
+		invalidMethodForTaskError, ok := err.(*applicationerror.InvalidURLForTaskError)
+		Ω(ok).To(BeTrue())
+
+		Ω(invalidMethodForTaskError.Message()).To(Equal("The url invalid-test.com is not valid for task."))
+		Ω(invalidMethodForTaskError.Code()).To(Equal(applicationerror.InvalidURLForTaskErrorCode))
+	})
+
+	It("should fail create task when dal save fail", func() {
+		context := context.Background()
+
+		createRequestDTO := servicedto.CreateTaskDTO{
+			Method: "POST",
+			URL:    "http://test.com",
+			Headers: map[string][]string{
+				"test": {"test"},
+			},
+			Body: map[string]interface{}{
+				"test": "test",
+			},
+		}
+
+		mockDALInterface.On(
+			"Save",
+			context,
+			mock.MatchedBy(func(actualTask *entity.Task) bool {
+				return actualTask.MethodToString() == createRequestDTO.Method &&
+					actualTask.URL == createRequestDTO.URL &&
+					reflect.DeepEqual(actualTask.RequestHeadersToMap(), createRequestDTO.Headers) &&
+					reflect.DeepEqual(actualTask.RequestBodyToMap(), createRequestDTO.Body) &&
+					actualTask.ID == 0 &&
+					actualTask.Status == entity.StatusNew &&
+					actualTask.ResponseStatusCodeToInt() == 0 &&
+					actualTask.ResponseContentLengthToInt() == 0 &&
+					actualTask.CreatedAt != time.Time{} &&
+					actualTask.UpdatedAt != time.Time{}
+			}),
+		).Return(errors.New("error test"))
+
+		task, err := taskService.CreateTask(context, &createRequestDTO)
+		Ω(err).ToNot(BeNil())
+		Ω(task).To(BeNil())
+
+		Ω(err.Error()).To(Equal("error test"))
 	})
 })
